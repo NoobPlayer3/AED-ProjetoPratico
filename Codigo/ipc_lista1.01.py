8617aea786909827fbdc39187cff88bb1601c8a7
NoobPlayer3 <NoobPlayer3@gmail.com>
2016-04-27 16:36:29 -0400

    ultima atualizacao

--- Codigo/ipc_lista1.01.py
+++ Codigo/ipc_lista1.01.py
@@ -1,11 +1,72 @@
+#include <iostream>
 
-#Lista de Estrutura Sequencial
-#Professor: Jucimar Junior
-#Alexander Emerson Batalha Carlos - 1615310051
-#Any Mendes Carvalho - 1615310044
-#Ariel Guilherme Rocha Capistrano - 1615310029
-#Beatriz Pessoa Longato - 1615310001
-#Nickso Patrick Façanha Calheiros - 1615310059
+class Conteiner{
+int nome;
+};
+
+class No{
+public:
+    Conteiner item;
+    No *prox;
+
+};
 
 
-print("Hello World")
+class Pilha{
+
+public:
+    No *topo,*fundo;
+    Pilha *prox;
+
+    Pilha(){
+        fundo = new No();
+        fundo->prox= NULL;
+        topo = fundo;
+        prox = NULL;
+    }
+
+    void empilhar(Pilha P, Conteiner x){
+        No *aux;
+        aux = new No();
+        P.topo->item = x;
+        aux->prox = P.topo;
+        P.topo = aux;
+    }
+
+    void desempilhar(Pilha P, Conteiner x){
+        No *aux;
+        aux = P.topo;
+        P.topo = P.topo->prox;
+        x = P.topo->item;
+        delete(aux);
+    }
+    
+    void busca(Pilha P, Conteiner x){
+        
+    }
+};
+
+class Lista{
+
+Pilha *prim,*ult;
+
+    void cria(Lista L, Pilha P){
+        L.prim = new Pilha(P);
+        L.ult = L.prim;
+    }
+    void insere(Lista L, Pilha P){
+        L.ult->prox = new Pilha(P);
+        L.ult = L.ult->prox;
+        L.ult->prox = NULL;
+    }
+
+};
+
+
+using namespace std;
+
+int main()
+{
+
+
+}
